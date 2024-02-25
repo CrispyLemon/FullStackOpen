0de1e55950dd521ef940061831b8070f4dccf13b
Aryan Baibaswata <aryanb349@gmail.com>
2024-02-25 23:58:21 +0530

    saving progress

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -5,18 +5,29 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-
+  const [all, setAll] = useState(0)
 
   const handleGoodClick = () => {
-    setGood(good+1)
+    const updatedGood = good + 1
+    setGood(updatedGood)
+    setAll(n => updatedGood + bad + neutral)
+    console.log(all)
   }
 
   const handleNeutralClick = () => {
-    setNeutral(neutral+1)
+    const updatedNeutral = neutral + 1
+    setNeutral(updatedNeutral)
+    setAll(n => updatedNeutral + good + bad)
+    console.log(all)
+
   }
 
   const handleBadClick = () => {
-    setBad(bad+1)
+    const updatedBad = bad + 1
+    setBad(updatedBad)
+    setAll(n => updatedBad + good + neutral)
+    console.log(all)
+
   }
 
 

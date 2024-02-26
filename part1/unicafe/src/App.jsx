bd19d1813b8331dda42d0ab19d3698f22e939312
CrispyLemon <aryanb349@gmail.com>
2024-02-26 13:51:07 +0530

    commit: solved till exercise 1.8

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,23 +1,43 @@
 import { useState } from 'react'
 
+const Statistics = (props) => {
+  const {Good, Bad, Neutral, All, Percentage} = props
+  return (
+    <>
+    <h1>statistics</h1>
+      <p>
+        good {Good} <br />
+        neutral {Neutral} <br />
+        bad {Bad} <br />
+        all {All} <br />
+        total {Percentage}% <br />
+      </p>
+    </>
+  )
+}
+
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
   const [all, setAll] = useState(0)
+  const percentage = all ? (good / all) * 100 : 0;
 
   const handleGoodClick = () => {
     const updatedGood = good + 1
     setGood(updatedGood)
-    setAll(n => updatedGood + bad + neutral)
+    console.log(updatedGood)
+    const updatedAll = updatedGood + bad + neutral
+    setAll(n => updatedAll)
     console.log(all)
   }
 
   const handleNeutralClick = () => {
     const updatedNeutral = neutral + 1
     setNeutral(updatedNeutral)
-    setAll(n => updatedNeutral + good + bad)
+    const updatedAll = updatedNeutral + good + bad
+    setAll(n => updatedAll)
     console.log(all)
 
   }
@@ -25,12 +45,12 @@ const App = () => {
   const handleBadClick = () => {
     const updatedBad = bad + 1
     setBad(updatedBad)
-    setAll(n => updatedBad + good + neutral)
+    const updatedAll = updatedBad + good + neutral
+    setAll(n => updatedAll)
     console.log(all)
-
   }
-
-
+  
+  
   return (
     <div>
       <h1>give feedback</h1>
@@ -43,12 +63,7 @@ const App = () => {
       <button onClick={handleBadClick}>
         Bad
       </button>
-      <h1>statistics</h1>
-      <p>
-        good {good} <br />
-        neutral {neutral} <br />
-        bad {bad} <br />
-      </p>
+      <Statistics Good={good} Neutral={neutral} Bad={bad} All={all} Percentage={percentage} />
     </div>
   )
 }

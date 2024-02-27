00a2b1270dc7b517a3dc656f8deb3bce110a9765
CrispyLemon <aryanb349@gmail.com>
2024-02-27 13:10:42 +0530

    commit : till exercise 1.11

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,18 +1,49 @@
 import { useState } from 'react'
 
+const StatisticLine = (props) => {
+  return(
+  <>
+    <tbody>
+    <tr><td>{props.text}</td> <td>{props.value}</td></tr><br />
+    </tbody>
+  </>
+  )
+}
+
 const Statistics = (props) => {
-  const {Good, Bad, Neutral, All, Percentage} = props
+  const {Good, Bad, Neutral, All, average, Percentage} = props
+  if (All == 0){
+    return(
+      <>
+        <h1>Statistics</h1>
+        <p>No feedback given.</p>
+      </>
+    )
+  }
+  else{
+  const percval = Percentage + "%"
   return (
     <>
     <h1>statistics</h1>
-      <p>
-        good {Good} <br />
-        neutral {Neutral} <br />
-        bad {Bad} <br />
-        all {All} <br />
-        total {Percentage}% <br />
-      </p>
+      <table>
+        <StatisticLine text = "good" value = {Good} />
+        <StatisticLine text = "bad" value = {Bad} />
+        <StatisticLine text = "neutral" value = {Neutral} />
+        <StatisticLine text = "all" value = {All} />
+        <StatisticLine text = "average" value = {average} />
+        <StatisticLine text = "percentage" value = {percval} />
+      </table>
     </>
+  )
+  }
+}
+
+  
+const Button = ({ handleClick, text }) => {
+  return (
+    <button onClick={handleClick}>
+      {text}
+    </button>
   )
 }
 
@@ -23,47 +54,35 @@ const App = () => {
   const [bad, setBad] = useState(0)
   const [all, setAll] = useState(0)
   const percentage = all ? (good / all) * 100 : 0;
-
+  const average = all ? ((good - bad) / all) : 0
   const handleGoodClick = () => {
     const updatedGood = good + 1
     setGood(updatedGood)
-    console.log(updatedGood)
     const updatedAll = updatedGood + bad + neutral
-    setAll(n => updatedAll)
-    console.log(all)
+    setAll(updatedAll)
   }
 
   const handleNeutralClick = () => {
     const updatedNeutral = neutral + 1
     setNeutral(updatedNeutral)
-    const updatedAll = updatedNeutral + good + bad
-    setAll(n => updatedAll)
-    console.log(all)
-
+    const updatedAll = updatedNeutral + bad + good
+    setAll(updatedAll)
   }
 
   const handleBadClick = () => {
     const updatedBad = bad + 1
-    setBad(updatedBad)
+    setBad(n => updatedBad)
     const updatedAll = updatedBad + good + neutral
-    setAll(n => updatedAll)
-    console.log(all)
+    setAll(updatedAll)
   }
-  
-  
+
   return (
     <div>
       <h1>give feedback</h1>
-      <button onClick={handleGoodClick}>
-        Good
-      </button>
-      <button onClick={handleNeutralClick}>
-        Neutral
-      </button>
-      <button onClick={handleBadClick}>
-        Bad
-      </button>
-      <Statistics Good={good} Neutral={neutral} Bad={bad} All={all} Percentage={percentage} />
+      <Button handleClick={handleGoodClick} text="Good" />
+      <Button handleClick={handleNeutralClick} text="Neutral" />
+      <Button handleClick={handleBadClick} text="Bad" />
+      <Statistics Good={good} Neutral={neutral} Bad={bad} All={all} average = {average} Percentage={percentage} />
     </div>
   )
 }

2bc10920014da5d2667bfffb9ee93bd5235e1d98
CrispyLemon <aryanb349@gmail.com>
2024-06-16 20:21:44 +0530

    finished part4.1-4.2

---
+++ part4/bloglist/app.js
@@ -0,0 +1,30 @@
+const config = require('./utils/config')
+const logger = require('./utils/logger')
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const mongoose = require('mongoose')
+const blogsRouter = require('./controllers/blogs')
+const middleware = require('./utils/middleware')
+const mongoUrl = config.MONGODB_URI
+
+  mongoose.connect(mongoUrl)
+    .then(
+      logger.info('Connected to MongoDB @', mongoUrl)
+    )
+    .catch(error => {
+      logger.error('Error connecting to MongoDB:', error.message)
+    }
+    )
+
+
+
+app.use(cors())
+app.use(express.json())
+app.use(middleware.requestLogger)
+
+app.use('/api/blogs', blogsRouter)
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
+
+module.exports = app
